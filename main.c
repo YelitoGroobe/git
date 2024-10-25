24fb456571c70a5fa3b07e36c661501e5e30e77d
YelitoGroobe <255742@edu.p.lodz.pl>
2024-10-25 12:58:05 +0200

    02 Zmiana metody Policzx

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int main(void) {
     int a;
     int b;
     printf("Hello, World!\n");
-    printf("Policz %d + %d = %d", a, b, a + b);
+    printf("Policz %d * %d = %d", a, b, a * b);
     return 0;
 }
